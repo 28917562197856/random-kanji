be69e85bfb5e07572951acbbdd260c8946951b57
28917562197856 <twoeightnineone2891@gmail.com>
2019-12-22 11:20:21 +0100

    added option to change # of kanji

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,6 @@
 import React, { useReducer } from "react";
 import data from "../rtk-kanji.json";
 
-for (let e of data) if (typeof e === "boolean") console.log(e);
-
 type Kanji = {
   keyword: string;
   kanji: string;
@@ -12,36 +10,40 @@ type State = {
   score: number;
   kanji: Kanji[];
   selectedKanji: Kanji;
+  numOfKanji: number;
 };
 
 type Action = {
   type: "select";
   score?: number;
+  numOfKanji?: number;
 };
 
-const take = (): Kanji[] => {
+const take = (n: number): Kanji[] => {
   let shuffled: any = data;
-  return shuffled.sort(() => 0.5 - Math.random()).slice(0, 18);
+  return shuffled.sort(() => 0.5 - Math.random()).slice(0, n);
 };
 
 const init = () => {
-  const kanji = take();
+  const kanji = take(12);
   const selectedKanji = kanji[(kanji.length * Math.random()) << 0];
   return {
     score: 0,
     kanji,
-    selectedKanji
+    selectedKanji,
+    numOfKanji: 12
   };
 };
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case "select": {
-      const newKanji = take();
+      const newKanji = take(action.numOfKanji || state.numOfKanji);
       return {
         score: action.score || state.score,
         kanji: newKanji,
-        selectedKanji: newKanji[(newKanji.length * Math.random()) << 0]
+        selectedKanji: newKanji[(newKanji.length * Math.random()) << 0],
+        numOfKanji: action.numOfKanji || state.numOfKanji
       };
     }
     default: {
@@ -64,10 +66,24 @@ export const App: React.FC = () => {
         justifyItems: "center"
       }}
     >
+      <select
+        className="absolute mt-6 lg:mt-8 ml-8 px-2 py-1"
+        onChange={e => {
+          dispatch({ type: "select", numOfKanji: parseInt(e.target.value) });
+        }}
+      >
+        {[...Array(21).keys()]
+          .map(x => x + 4)
+          .map(x => (
+            <option selected={x === 12 ? true : undefined} key={x}>
+              {x}
+            </option>
+          ))}
+      </select>
       <div className="my-2 text-3xl lg:text-6xl">{`${selectedKanji.keyword
         .charAt(0)
         .toUpperCase()}${selectedKanji.keyword.substring(1)}`}</div>
-      <div className="absolute top-0 right-0 mt-2 mr-8 text-3xl lg:mr-32 lg:text-6xl">
+      <div className="absolute right-0 mt-2 mr-8 text-3xl lg:mr-32 lg:text-6xl">
         {score}
       </div>
       <div className="text-6xl sm:w-auto lg:w-1/3 flex flex-wrap justify-center">
